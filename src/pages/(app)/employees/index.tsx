fcba5af42befff63f23381ba47b2d956a5fc9e1b
megatkhs <tarodino0526@gmail.com>
2023-07-30 23:47:29 +0900

    update: 従業員一覧の表示を調整

--- src/pages/(app)/employees/index.tsx
+++ src/pages/(app)/employees/index.tsx
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { SuspenseDepartmentLabel } from "@/components/domains/DepartmentLabel/DepartmentLabel";
 import { employeeSchema, type Employee } from "@/models/employee";
 import { fetcher } from "@/utils/fetcher";
+import styles from "./index.module.css";
 import type { LoaderFunction } from "react-router-dom";
 
 export const Loader = (async () => {
@@ -26,24 +27,27 @@ export default function EmployeesPage() {
         <title>従業員一覧 | 社内管理システム</title>
       </Helmet>
 
-      <ul>
+      <ul className={styles.list}>
         {employees.map((employee) => (
-          <li key={employee.id}>
-            <Link to={`/employees/${employee.id}`}>
-              <dl>
-                <dt>名前</dt>
-                <dd>
-                  {employee.last_name} {employee.first_name}
-                </dd>
+          <li key={employee.id} className={styles.item}>
+            <Link className={styles.link} to={`/employees/${employee.id}`}>
+              <article className={styles.card}>
+                <hgroup>
+                  <p>
+                    <SuspenseDepartmentLabel id={employee.id} />
+                  </p>
+                  <h1 className={styles.name}>
+                    {employee.last_name} {employee.first_name}
+                  </h1>
+                </hgroup>
 
-                <dt>誕生日</dt>
-                <dd>{dayjs(employee.birthday).format("YYYY年MM月DD日")}</dd>
-
-                <dt>部署ID</dt>
-                <dd>
-                  <SuspenseDepartmentLabel id={employee.id} />
-                </dd>
-              </dl>
+                <p className={styles.birthday}>
+                  <time dateTime={employee.birthday}>
+                    {dayjs(employee.birthday).format("YYYY年MM月DD日")}
+                  </time>
+                  生まれ
+                </p>
+              </article>
             </Link>
           </li>
         ))}
